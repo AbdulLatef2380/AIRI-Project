d39dc709ab0526d4038906c9401f11ac537bbc86
AbdulLatef2380 <xwenbrr847@gmail.com>
2026-03-01 18:27:51 +0200

    Create IntentType.kt

---
+++ app/src/main/java/com/airi/assistant/accessibility/IntentType.kt
@@ -0,0 +1,10 @@
+package com.airi.assistant.accessibility
+
+enum class IntentType {
+    SUMMARIZE,         // تلخيص
+    CODE_ANALYSIS,     // تحليل كود
+    DEBUG_ERROR,       // حل مشكلة تقنية
+    BATTERY_DIAGNOSIS, // فحص بطارية
+    TRANSLATE,         // ترجمة
+    GENERAL            // سؤال عام
+}

9b2c95b28dd26c8eb5b41d40fac1199328a17b8e
AbdulLatef2380 <xwenbrr847@gmail.com>
2026-03-01 23:44:00 +0200

    Create UsageStatsDao.kt

---
+++ app/src/main/java/com/airi/assistant/data/UsageStatsDao.kt
@@ -0,0 +1,16 @@
+package com.airi.assistant.data
+
+import androidx.room.*
+
+@Dao
+interface UsageStatsDao {
+
+    @Query("SELECT * FROM usage_stats")
+    suspend fun getAll(): List<UsageStatEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(stat: UsageStatEntity)
+
+    @Query("UPDATE usage_stats SET usageCount = usageCount + 1, lastUsedTimestamp = :time WHERE suggestionText = :text")
+    suspend fun incrementUsage(text: String, time: Long)
+}

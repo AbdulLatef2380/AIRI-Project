9409ab755cf8a0cd40f560a5a71435224807f303
AbdulLatef2380 <xwenbrr847@gmail.com>
2026-03-02 19:01:55 +0200

    Create CommandRouter.kt

---
+++ app/src/main/java/com/airi/assistant/agent/command/CommandRouter.kt
@@ -0,0 +1,22 @@
+package com.airi.assistant.agent.command
+
+object CommandRouter {
+
+    fun execute(step: String): CommandResult {
+
+        return when (step) {
+
+            "LAUNCH_APP" ->
+                AccessibilityCommandBridge.launchApp()
+
+            "GO_BACK" ->
+                AccessibilityCommandBridge.performBack()
+
+            "TYPE_QUERY" ->
+                AccessibilityCommandBridge.typeText("Sample Query")
+
+            else ->
+                CommandResult(false, "Unknown step")
+        }
+    }
+}

d6ddbe189fb71bc5d38bca0649db4a2391d9232b
AbdulLatef2380 <xwenbrr847@gmail.com>
2026-03-03 12:22:56 +0200

    Update TaskChainer.kt

--- core/src/main/java/com/airi/core/chain/TaskChainer.kt
+++ core/src/main/java/com/airi/core/chain/TaskChainer.kt
@@ -1,5 +1,7 @@
 package com.airi.core.chain
 
+import kotlinx.coroutines.*
+
 class TaskChainer {
 
     private val goals = mutableListOf<AgentGoal>()
@@ -8,9 +10,24 @@ class TaskChainer {
         goals.add(goal)
     }
 
-    fun execute(executor: (AgentGoal) -> Unit) {
+    suspend fun execute(
+        executor: suspend (AgentGoal) -> Unit,
+        verifierProvider: (AgentGoal) -> (suspend () -> Boolean)
+    ) {
+
         for (goal in goals) {
+
             executor(goal)
+
+            val verifier = TemporalVerifier()
+
+            val result = verifier.verify(
+                expectedCondition = verifierProvider(goal)
+            )
+
+            if (result.state != GoalExecutionState.SUCCESS) {
+                break
+            }
         }
     }
 }

5c10ac1c622e3670395b6d82685aafbd231472af
AbdulLatef2380 <xwenbrr847@gmail.com>
2026-03-05 15:22:38 +0200

    Create BrainManager.kt

---
+++ app/src/main/java/com/airi/assistant/brain/BrainManager.kt
@@ -0,0 +1,23 @@
+package your.package.brain
+
+import android.util.Log
+
+object BrainManager {
+
+    fun processScreen(screenText: String) {
+
+        Log.d("AIRI_BRAIN", "Processing screen...")
+
+        if (screenText.contains("YouTube", true)) {
+
+            Log.d("AIRI_BRAIN", "Detected YouTube")
+
+        }
+
+        if (screenText.contains("Settings", true)) {
+
+            Log.d("AIRI_BRAIN", "Detected Settings")
+
+        }
+    }
+}

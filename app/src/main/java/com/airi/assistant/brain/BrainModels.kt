15eb6d183859811d9d746cbf8daee9d93ad08a23
AbdulLatef2380 <xwenbrr847@gmail.com>
2026-03-05 02:31:26 +0200

    Update BrainModels.kt

--- app/src/main/java/com/airi/assistant/brain/BrainModels.kt
+++ app/src/main/java/com/airi/assistant/brain/BrainModels.kt
@@ -1,10 +1,1 @@
 package com.airi.assistant.brain
-
-sealed class PlanStep {
-
-    data class Click(val target: String) : PlanStep()
-
-    object Scroll : PlanStep()
-
-    data class Wait(val millis: Long) : PlanStep()
-}

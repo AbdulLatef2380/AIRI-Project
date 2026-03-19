f7b858d290780d0a35f3981683d84e6aee47a39b
AbdulLatef2380 <xwenbrr847@gmail.com>
2026-03-19 03:24:21 +0200

    Create IntentType.kt

---
+++ core/intent/IntentType.kt
@@ -0,0 +1,19 @@
+package com.airi.assistant.core.intent
+
+enum class IntentType {
+    GENERAL,
+    CONVERSATION,
+    CODE_ANALYSIS,
+    DEBUG_ERROR,
+    SUMMARIZE,
+    SYSTEM_COMMAND,
+    APP_CONTROL,
+    SCREEN_ANALYSIS,
+    BATTERY_DIAGNOSIS,
+    CLICK,
+    CLICK_FIRST,
+    CLICK_INDEX,
+    TYPE,
+    BACK,
+    UNKNOWN
+}
